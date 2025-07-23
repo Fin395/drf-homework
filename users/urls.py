5d36ba5a440fad21c9420f772359f00d55b82539
Sergei Finogenov <serfinogenov@yandex.ru>
2025-07-23 20:53:15 +0300

    creating serializer, view and url for list of payments

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-
 from users.apps import UsersConfig
-from users.views import UserUpdateAPIView
+from users.views import UserUpdateAPIView, PaymentsListAPIView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
     path("user/update/<int:pk>/", UserUpdateAPIView.as_view(), name="user-update"),
+    path("payments/", PaymentsListAPIView.as_view(), name="payments-list"),
+
 ]
--- users/views.py
+++ users/views.py
@@ -1,9 +1,13 @@
 from rest_framework import generics
-
-from users.models import User
-from users.serializers import UserSerializer
+from users.models import User, Payments
+from users.serializers import UserSerializer, PaymentsSerializer
 
 
 class UserUpdateAPIView(generics.UpdateAPIView):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+
+
+class PaymentsListAPIView(generics.ListAPIView):
+    serializer_class = PaymentsSerializer
+    queryset = Payments.objects.all()
