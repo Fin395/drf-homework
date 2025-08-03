cff9ff46d987c0cdf4a353960c3382775cbf0e75
Sergei Finogenov <serfinogenov@yandex.ru>
2025-08-04 01:12:40 +0300

    creating methods for creation of the product, price and session

---
+++ users/services.py
@@ -0,0 +1,35 @@
+import stripe
+
+from config.settings import API_STRIPE_KEY
+
+
+stripe.api_key = API_STRIPE_KEY
+
+
+def create_stripe_product(product_name):
+    """Создаем продукт в stripe."""
+
+    product = stripe.Product.create(name=product_name)
+    return product
+
+
+def create_stripe_price(amount, product_name ):
+    """Создаем цену в stripe."""
+
+    price = stripe.Price.create(
+        currency="rub",
+        unit_amount=amount * 100,
+        product_data={"name": product_name},
+    )
+    return price
+
+
+def create_stripe_session(price):
+    """Создаем в stripe сессию для получения ссылки на оплату."""
+
+    session = stripe.checkout.Session.create(
+        success_url="https://127.0.0.1:8000/",
+        line_items=[{"price": price.get('id'), "quantity": 1}],
+        mode="payment",
+    )
+    return session.get('id'), session.get('url')
