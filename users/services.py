aee5692ea482a7a3f79b3a780548b825da93ef8e
Sergei Finogenov <serfinogenov@yandex.ru>
2025-08-04 04:09:16 +0300

    fixing the product name

--- users/services.py
+++ users/services.py
@@ -19,7 +19,7 @@ def create_stripe_price(amount, product):
     price = stripe.Price.create(
         currency="rub",
         unit_amount=amount * 100,
-        product_data={"name": product.get("id")},
+        product_data={"name": f"Курс {product.get("name")}"},
     )
     return price
 
