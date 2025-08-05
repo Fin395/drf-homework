62b46a3fb192ac53eac059fa441a0e5014317948
Sergei Finogenov <serfinogenov@yandex.ru>
2025-08-06 01:07:12 +0300

    fixing the field 'updated_at' for the Course model

--- materials/views.py
+++ materials/views.py
@@ -35,13 +35,14 @@ class CourseViewSet(ModelViewSet):
 
         return [permission() for permission in self.permission_classes]
 
-    def update(self, request, *args, **kwargs):
-        partial = kwargs.pop('partial', False)
-        instance = self.get_object()
-        serializer = self.get_serializer(instance, data=request.data, partial=partial)
-        serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
-        return Response({'message': 'курс обновлен'})
+    # def update(self, request, *args, **kwargs):
+    #     partial = kwargs.pop('partial', True)
+    #     instance = self.get_object()
+    #     instance.updated_at = datetime.now()
+    #     serializer = self.get_serializer(instance, data=request.data, partial=partial)
+    #     serializer.is_valid(raise_exception=True)
+    #     self.perform_update(serializer)
+    #     return Response(data=serializer.data)
 
 class LessonCreateAPIView(generics.CreateAPIView):
     serializer_class = LessonSerializer
