6429f37fef2abc6cec277f30f4576642cc06b9d8
Sergei Finogenov <serfinogenov@yandex.ru>
2025-08-07 01:19:04 +0300

    adding docstring to the task

--- materials/tasks.py
+++ materials/tasks.py
@@ -7,6 +7,7 @@ from materials.models import Course, Subscription
 
 @shared_task
 def send_update_mail(course_id):
+    """Отправляет подписчикам сообщение об обновлении курса."""
     course = Course.objects.get(pk=course_id)
     subscriptions = Subscription.objects.filter(course=course)
 
