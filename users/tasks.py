6e1b10d9b133d28c30da525b0433f68b41f68587
Sergei Finogenov <serfinogenov@yandex.ru>
2025-08-07 02:45:16 +0300

    adding docstrings

--- users/tasks.py
+++ users/tasks.py
@@ -8,6 +8,7 @@ from users.models import User
 
 @shared_task
 def deactivate_inactive_users():
+    """Деактивирует пользователя, проходившего авторизацию более месяца назад."""
     month_ago = timezone.now().today() - timedelta(days=30)
     inactive_users = User.objects.filter(is_active=True).filter(last_login__isnull=False).filter(last_login__lt=month_ago)
 
