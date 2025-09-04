55fc02f7d817a904eb5e87dca4e79a66d22e6ca7
Sergei Finogenov <serfinogenov@yandex.ru>
2025-09-05 02:01:29 +0300

    deploy

--- config/settings.py
+++ config/settings.py
@@ -147,11 +147,3 @@ EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 
 SERVER_EMAIL = EMAIL_HOST_USER
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-
-if "test" in sys.argv:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "test_db_sqlite3",
-        }
-    }
