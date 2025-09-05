55ae3c0e5b71c9269811d0e33d8e5de54d4d0bf2
Sergei Finogenov <serfinogenov@yandex.ru>
2025-09-05 22:51:20 +0300

    deploy

--- config/settings.py
+++ config/settings.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 from datetime import timedelta
 
-load_dotenv()
+load_dotenv(override=True)
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
